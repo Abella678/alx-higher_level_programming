d7b7636b7cc617c62077819ca1ed0fe15ed6c802
Abel Mengistu <abelmemory0@gmail.com>
2022-07-16 02:38:59 -0700

    Task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     import sys
 
-
-if len(sys.argv) == 1:
-    print("{:d} arguments.".format(len(sys.argv) - 1))
-if len(sys.argv) == 2:
-    print("{:d} argument:".format(len(sys.argv) - 1))
-if len(sys.argv) > 2:
-    print("{:d} arguments:".format(len(sys.argv) - 1))
-if len(sys.argv):
-    for i in range(1, len(sys.argv)):
-        print("{:d}:".format(i), sys.argv[i])
+    m = len(sys.argv) - 1
+    if m == 0:
+        print("0 arguments.")
+    elif m == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(m))
+    for x in range(m):
+        print("{}: {}".format(x + 1, sys.argv[x + 1]))
