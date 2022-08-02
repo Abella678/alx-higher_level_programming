48fb7514a57b63520b205f8c67834152400b2858
Abel Mengistu <abelmemory0@gmail.com>
2022-08-02 07:28:17 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""write_file module"""
+""" Defines a text file-reading function"""
 
 
-def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8) and returns the number of
-        characters written"""
-    with open(filename, "w", encoding="utf-8") as content:
-        return content.write(text)
+def read_file(filename=""):
+    """ Print the contents of file to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
